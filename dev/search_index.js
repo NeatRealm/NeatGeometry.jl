8dc59be1bd3fdb1604fe60d733c73a28ea6a254b
Documenter.jl <documenter@juliadocs.github.io>
2023-09-12 23:06:59 +0000

    build based on fe217cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeatGeometry","category":"page"},{"location":"#NeatGeometry","page":"Home","title":"NeatGeometry","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeatGeometry.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeatGeometry]","category":"page"}]
+}
